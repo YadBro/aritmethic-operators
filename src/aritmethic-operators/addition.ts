7e5c83c6954be1ba30a814fab9072f4c621bb186
YadBro <ncada708@gmail.com>
2022-07-18 01:00:38 +0700

    my new modules

---
+++ src/aritmethic-operators/addition.ts
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num1  add number 1
+ * @param {number} num2 add number 2
+ * @returns Returns result your addition
+ * @example
+ * addition(1, 1);
+ * // => 2
+ */
+
+function addition(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+export {addition}
---
+++ test/arithmethic-operators.test.js
@@ -0,0 +1,7 @@
+import {addition} from '@yadiganteng32/aritmetic-operators';
+
+test('should first', () => { 
+  const add = addition(1, 1);
+
+  expect(add).toBe(2);
+})
