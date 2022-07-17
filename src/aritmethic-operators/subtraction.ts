8690b1753356f2c902c06981c5813c573df54ce0
YadBro <ncada708@gmail.com>
2022-07-18 01:43:48 +0700

    new feature

---
+++ src/aritmethic-operators/subtraction.ts
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num1  add number 1
+ * @param {number} num2 add number 2
+ * @returns Returns result your subtraction
+ * @example
+ * subtraction(1, 1);
+ * // => 0
+ */
+
+function subtraction(num1: number, num2: number) {
+  return num1 - num2;
+}
+
+export {subtraction}
