da6b5e7139896315e4d315300b8108a8c15d5dab
YadBro <ncada708@gmail.com>
2022-07-18 01:46:46 +0700

    new test and update package

--- test/arithmethic-operators.test.js
+++ test/arithmethic-operators.test.js
@@ -1,7 +1,13 @@
-import {addition} from '@yadiganteng32/aritmetic-operators';
+import {addition, subtraction} from '@yadiganteng32/aritmetic-operators';
 
-test('should first', () => { 
+test('should add 2 numbers', () => { 
   const add = addition(1, 1);
 
   expect(add).toBe(2);
+})
+
+test('should less 2 numbers', () => { 
+  const less = subtraction(5, 1);
+
+  expect(less).toBe(4);
 })
